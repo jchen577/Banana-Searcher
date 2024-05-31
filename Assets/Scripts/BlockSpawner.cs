26e471e93e323e3f23d4315ec543d2bce3b8743b
Ashley Knapp <ajknapp@ucsc.edu>
2024-05-30 18:57:55 -0700

    LongCube when placed now updates the navmesh as an obstacle

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -11,22 +11,21 @@ public class BlockSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		// Using a similar method as clickToMove for the bananas
+	// Using a similar method as clickToMove for the bananas
 		if (Input.GetMouseButtonDown(1))
 		{
-			if (Input.GetMouseButtonDown(1))
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Input.mousePosition.y < 1200)
 			{
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				if (Input.mousePosition.y < 1200)
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
 				{
-					RaycastHit hit;
-					if (Physics.Raycast(ray, out hit))
-					{
-						int randRotate = Random.Range(0, 180);
-						Quaternion rot = Quaternion.Euler(0, randRotate, 0);
-						Instantiate(LongCube, hit.point, rot);
-						//Debug.Log("Instantiating with position: "+hit.point+" and rotation: "+randRotate);
-					}
+					// Place down a randomly rotated block
+					// int randRotate = Random.Range(0, 180);
+					// Quaternion rot = Quaternion.Euler(0, randRotate, 0);
+
+					Quaternion rot = Quaternion.Euler(0, 0, 0); // No rotation
+					Instantiate(LongCube, hit.point, rot);
 				}
 			}
 		}
