3b434e381decc71367db149647f99a766dc13e4b
Ashley Knapp <ajknapp@ucsc.edu>
2024-06-03 05:53:03 -0700

    added a reset button that stops monkey movement, allows banana movement, clears placed blocks. added functions to some scripts that allow the object attached to be enable/disabled. Also put limits on the camera movement

--- Assets/Scripts/movement/cameraMovement.cs
+++ Assets/Scripts/movement/cameraMovement.cs
@@ -14,11 +14,11 @@ public class cameraMovement : MonoBehaviour
         float newX = transform.position.x + xAxisValue;
         float newZ = transform.position.z + zAxisValue;
 
+        // Limit the camera movement (to keep the game space on screen at all times)
         if (newX > -6 && newX < 30 && newZ > -15 && newZ < 20)
         {
             transform.position = new Vector3(newX, transform.position.y, newZ);
         }
         
-        // notes: camera should have limits so you cant move it tooo far away from play area -ashley
         }
 }
